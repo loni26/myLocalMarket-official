750c4100c48a1d920cc40dce81b021bf5bb1f03b
Nilo Castillo <nilo.castill026@gmail.com>
2020-12-15 14:43:17 +0100

    init commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCX39mvVKvqNIl4YMHvMDiAJal8BBmA-yU",
+    authDomain: "mylocalmarket-1b4d8.firebaseapp.com",
+    databaseURL: "https://mylocalmarket-1b4d8.firebaseio.com",
+    projectId: "mylocalmarket-1b4d8",
+    storageBucket: "mylocalmarket-1b4d8.appspot.com",
+    messagingSenderId: "240708036063",
+    appId: "1:240708036063:web:696bcf8493a70dbd0ad06a"
+  }
 };
 
 /*
